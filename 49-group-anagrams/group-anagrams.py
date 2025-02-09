e974f6f313d30dc5680f9d0b2e049744965113f5
ArC48 <74317740+ArC48@users.noreply.github.com>
2025-02-09 22:26:21 +0400

    Time: 26 ms (12.74%) | Memory: 22.6 MB (30.04%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,16 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mapping = {}
+        mapping = defaultdict(list)
         for word in strs:
             curr_map = [0] * 26
 
             for ch in word:
                 curr_map[ord(ch) - ord("a")] += 1
 
-            if tuple(curr_map) in mapping:
-                mapping[tuple(curr_map)].append(word)
-            else:
-                mapping[tuple(curr_map)] = [word]
+            mapping[tuple(curr_map)].append(word)
         
         result = []
         for key in mapping:
