4030a4467ee4e6c949fca447ef67a8c37eda8384
ArC48 <74317740+ArC48@users.noreply.github.com>
2025-03-23 15:01:57 +0400

    Time: 13 ms (13.74%) | Memory: 24.5 MB (7.57%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,28 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counts = {}
+
+        for num in nums:
+            counts[num] = counts.get(num, 0) + 1
+        
+        # bucket sort
+        buckets = []
+        for _ in range(len(nums) + 1): buckets.append([])
+
+        for element, count in counts.items():
+            buckets[count].append(element)
+        print(buckets)
+        result = []
+
+        for i in range(len(buckets) - 1, 0, -1):
+            if buckets[i]:
+                for elem in buckets[i]:
+                    result.append(elem)
+                    k -= 1
+                    if k == 0:
+                        break
+            if not k:
+                break
+                
+        return result
+                
