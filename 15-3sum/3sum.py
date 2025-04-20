cf1743f17f9092d225401a99bda2b44e07bf3bca
ArC48 <74317740+ArC48@users.noreply.github.com>
2025-04-20 19:09:48 +0400

    Time: 598 ms (46.17%) | Memory: 20.5 MB (84.60%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -13,9 +13,9 @@ class Solution:
                 curr = nums[left] + nums[right] + nums[i]
                 if curr == 0:
                     result.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1
+                    right -= 1
+                    while nums[right] == nums[right + 1] and left < right:
+                        right -= 1
                 elif curr < 0:
                     left += 1
                 else:
