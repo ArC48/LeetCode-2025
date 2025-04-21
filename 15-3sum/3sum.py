4be187d69b5f489406cded38c125dd2ad4a0e2b7
ArC48 <74317740+ArC48@users.noreply.github.com>
2025-04-21 15:21:17 +0400

    Time: 995 ms (18.66%) | Memory: 21.2 MB (16.44%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,24 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        result = []
-
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
+        seen = set()
+        res = set()
+        for i, num in enumerate(nums):
+            if num in seen:
                 continue
+            seen.add(num)
+            r = self.twoSum(i, nums, -num)
+            if r:
+                for elem in r:
+                    res.add(tuple(sorted(elem)))
+        return list(res)
 
-            left = i + 1
-            right = len(nums) - 1
-            while left < right:
-                curr = nums[left] + nums[right] + nums[i]
-                if curr == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-                    right -= 1
-                    while nums[right] == nums[right + 1] and left < right:
-                        right -= 1
-                elif curr < 0:
-                    left += 1
-                else:
-                    right -= 1
 
-        return result
+    def twoSum(self, startIndex, nums, target) -> List[int]:
+        seen = set()
+        used = set()
+        res = []
+        for i in range(startIndex + 1, len(nums)):
+            if nums[i] in used:
+                continue
+            need = target - nums[i]
+            if need in seen:
+                res.append([-target, need, nums[i]])
+                used.add(nums[i])
+            seen.add(nums[i])
+        return res
