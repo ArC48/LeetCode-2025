48c82a004c2107602fd02ca5eeabd6f233db121b
ArC48 <74317740+ArC48@users.noreply.github.com>
2025-04-25 11:45:36 +0400

    Time: 16 ms (42.97%) | Memory: 21 MB (50.62%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,19 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mapping = {}
+        groups = {}
+
         for word in strs:
-            curr_map = [0] * 26
-
-            for ch in word:
-                curr_map[ord(ch) - ord("a")] += 1
-
-            if tuple(curr_map) in mapping:
-                mapping[tuple(curr_map)].append(word)
-            else:
-                mapping[tuple(curr_map)] = [word]
+            key = str(sorted(word))
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(word)
         
-        result = []
-        for key in mapping:
-            result.append(mapping[key])
-        
-        return result
+        res = []
+        for key in groups:
+            res.append(groups[key])
+        return res
