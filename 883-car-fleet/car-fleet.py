332f155ae54d3f40561abdd4e93ed6b8aca5a46c
ArC48 <74317740+ArC48@users.noreply.github.com>
2025-05-12 16:12:10 +0400

    Time: 171 ms (48.94%) | Memory: 39 MB (45.66%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -4,12 +4,12 @@ class Solution:
         pos_spd.sort(key=lambda x: x[0])
 
         fleets = 1
-        stack = [(target - pos_spd[-1][0]) / pos_spd[-1][1]]
+        current_fleet_time = (target - pos_spd[-1][0]) / pos_spd[-1][1]
         for i in range(len(pos_spd) - 2, -1, -1):
             p, s = pos_spd[i]
             time = (target - p) / s
-            if time > stack[-1]:
-                stack.append(time)
+            if time > current_fleet_time:
+                current_fleet_time = time
                 fleets += 1
             
         return fleets
