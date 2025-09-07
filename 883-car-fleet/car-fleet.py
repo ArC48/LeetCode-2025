abfd3be1030d3e950856141a10aeebf17a981dfb
ArC48 <74317740+ArC48@users.noreply.github.com>
2025-09-07 13:44:25 +0400

    Time: 196 ms (38.25%) | Memory: 41.6 MB (16.76%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,16 +1,14 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pos_spd = [(position[i], speed[i]) for i in range(len(position))]
-        pos_spd.sort(key=lambda x: x[0])
-
-        last_speed = (target - pos_spd[-1][0]) / pos_spd[-1][1]
+        cars = sorted(zip(position, speed))
+        times = [ (target - car[0]) / car[1] for car in cars ]
         fleets = 1
-
-        for i in range(len(pos_spd) - 2, -1, -1):
-            current_speed = (target - pos_spd[i][0]) / pos_spd[i][1]
-            if current_speed > last_speed:
+        curr_fleet_time = times.pop()
+        while times:
+            time = times.pop()
+            if time > curr_fleet_time:
                 fleets += 1
-                last_speed = current_speed
+                curr_fleet_time = time
 
         return fleets
 
